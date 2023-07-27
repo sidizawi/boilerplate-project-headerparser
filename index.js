45e6cf467251afbe6de58cb45b874af02b67c5a6
sidi <zawisidim@gmail.com>
2023-07-27 22:44:24 +0200

    finish

--- index.js
+++ index.js
@@ -19,9 +19,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+const fs = require('fs');
+
+// API endpoint...
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    "ipaddress": req.headers['x-forwarded-for'],
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  })
 });
 
 // listen for requests :)
